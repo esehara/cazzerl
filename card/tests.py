ee82477193ba4e63a84ce5ca4db59c67811337fa
esehara <esehara@gmail.com>
2012-07-31 12:40:45 +0900

    Jenkins Test

--- card/tests.py
+++ card/tests.py
@@ -4,5 +4,5 @@ class TestCard(object):
     def test_is_running(self):
         nt.assert_equal(1,1)
 
-    def test_is_plus_one(self):
-        nt.assert_equal("1","1")
+    def get_one_card(self):
+        nt.assert_equal("1","2")
