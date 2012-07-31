6515761d4e6d63c6ddb408ac5a3799d123c7d49d
esehara <esehara@gmail.com>
2012-07-31 11:45:56 +0900

    Setting Fix

--- cazzerl/settings.py
+++ cazzerl/settings.py
@@ -118,7 +118,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'card',
     'django_nose',
-    'django_jenkins',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
